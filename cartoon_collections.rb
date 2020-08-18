9bdce9e9193dc6a7a628c6c5ef149a2a65211ab3
Ange Batie <ange.batie@gmail.com>
2020-08-18 01:48:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.include? { |element| element == .include(valid_calls)
+  planeteer_calls.include? { |element| element == valid_calls.include?(element)
 end
