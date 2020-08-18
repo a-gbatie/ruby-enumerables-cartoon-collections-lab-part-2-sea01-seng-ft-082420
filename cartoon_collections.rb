3ff6f9b6fffbf4608a919b5250bede46f0647248
Ange Batie <ange.batie@gmail.com>
2020-08-18 01:25:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  array.capitalize { |element| element + "!" }l
+  array.capitalize { |element| element + "!" }
 end
 
 def long_planeteer_calls(planeteer_calls)
